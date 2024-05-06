598fe9ceb984961f4af4073df9a4ec33d2dd53ac
JihadNihadAwad <162121788+JihadNihadAwad@users.noreply.github.com>
2024-05-06 14:14:10 +0300

    Created demo UI

--- src/main/java/com/github/lamico/gui/GUIApplication.java
+++ src/main/java/com/github/lamico/gui/GUIApplication.java
@@ -1,31 +1,33 @@
 package com.github.lamico.gui;
 
-import com.github.lamico.gui.controllers.MainController;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.Scene;	
 import javafx.stage.Stage;
 
 public class GUIApplication extends Application {
 
-	// Run from a different class (Main)
+	/* Run from a different class (Main.java) */
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("/fxml/root.fxml"));
+		FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("/fxml/demo.fxml"));
 		Parent mainRoot = mainLoader.load();
 		// MainController mainController = mainLoader.getController();
+		
 
-		Scene scene = new Scene(mainRoot, 300, 200);
-
+		Scene scene = new Scene(mainRoot);
+		
+		scene.getStylesheets().add(getClass().getResource("/css/root.css").toExternalForm());
+		
 		primaryStage.setScene(scene);
-
-		primaryStage.setTitle("HelloWorld");
+		primaryStage.setMinHeight(563);
+		primaryStage.setMinWidth(934);
+		primaryStage.setTitle("Lamico Demo");
 
 		primaryStage.show();
 	}
--- src/main/java/com/github/lamico/gui/controllers/MainController.java
+++ src/main/java/com/github/lamico/gui/controllers/MainController.java
@@ -2,7 +2,13 @@ package com.github.lamico.gui.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
@@ -13,7 +19,111 @@ public class MainController {
     private URL location;
 
     @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnInsert;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> tvBank;
+
+    @FXML
+    private TableColumn<?, ?> tvBirthDate;
+
+    @FXML
+    private TableColumn<?, ?> tvDepartment;
+
+    @FXML
+    private TableView<?> tvEmployee;
+
+    @FXML
+    private TableColumn<?, ?> tvHireDate;
+
+    @FXML
+    private TableColumn<?, ?> tvName;
+
+    @FXML
+    private TableColumn<?, ?> tvPosition;
+
+    @FXML
+    private TableColumn<?, ?> tvSSN;
+
+    @FXML
+    private TableColumn<?, ?> tvSalary;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtBankName;
+
+    @FXML
+    private TextField txtBirthDate;
+
+    @FXML
+    private TextField txtDepartment;
+
+    @FXML
+    private TextField txtHireDate;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPosition;
+
+    @FXML
+    private TextField txtSSN;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void deleteEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRowSelection(MouseEvent event) {
+
+    }
+
+    @FXML
+    void insertEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert btnDelete != null : "fx:id=\"btnDelete\" was not injected: check your FXML file 'root.fxml'.";
+        assert btnInsert != null : "fx:id=\"btnInsert\" was not injected: check your FXML file 'root.fxml'.";
+        assert btnUpdate != null : "fx:id=\"btnUpdate\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvBank != null : "fx:id=\"tvBank\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvBirthDate != null : "fx:id=\"tvBirthDate\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvDepartment != null : "fx:id=\"tvDepartment\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvEmployee != null : "fx:id=\"tvEmployee\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvHireDate != null : "fx:id=\"tvHireDate\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvName != null : "fx:id=\"tvName\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvPosition != null : "fx:id=\"tvPosition\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvSSN != null : "fx:id=\"tvSSN\" was not injected: check your FXML file 'root.fxml'.";
+        assert tvSalary != null : "fx:id=\"tvSalary\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtAddress != null : "fx:id=\"txtAddress\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtBankName != null : "fx:id=\"txtBankName\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtBirthDate != null : "fx:id=\"txtBirthDate\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtDepartment != null : "fx:id=\"txtDepartment\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtHireDate != null : "fx:id=\"txtHireDate\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtName != null : "fx:id=\"txtName\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtPosition != null : "fx:id=\"txtPosition\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtSSN != null : "fx:id=\"txtSSN\" was not injected: check your FXML file 'root.fxml'.";
+        assert txtSalary != null : "fx:id=\"txtSalary\" was not injected: check your FXML file 'root.fxml'.";
 
     }
 
