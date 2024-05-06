09eb7badc617fb57d302f0deba040f2b5a423626
JihadNihadAwad <162121788+JihadNihadAwad@users.noreply.github.com>
2024-05-06 11:49:24 +0300

    Created root FXML

--- src/main/java/com/github/lamico/gui/GUIApplication.java
+++ src/main/java/com/github/lamico/gui/GUIApplication.java
@@ -1,9 +1,11 @@
 package com.github.lamico.gui;
 
+import com.github.lamico.gui.controllers.MainController;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GUIApplication extends Application {
@@ -15,16 +17,15 @@ public class GUIApplication extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Label helloLabel = new Label("Hello, World!");
+		FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("/fxml/root.fxml"));
+		Parent mainRoot = mainLoader.load();
+		// MainController mainController = mainLoader.getController();
 
-		StackPane root = new StackPane();
-		root.getChildren().add(helloLabel);
-
-		Scene scene = new Scene(root, 300, 200);
+		Scene scene = new Scene(mainRoot, 300, 200);
 
 		primaryStage.setScene(scene);
 
-		primaryStage.setTitle("HelloWorldApp");
+		primaryStage.setTitle("HelloWorld");
 
 		primaryStage.show();
 	}
