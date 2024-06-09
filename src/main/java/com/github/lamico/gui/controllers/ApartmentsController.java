53ff2da67be47dad8360bf35eeea94c999f63936
Jihad Awad <162121788+JihadNihadAwad@users.noreply.github.com>
2024-06-09 08:11:30 +0300

    Main screen, update and delete for apartments finished

--- src/main/java/com/github/lamico/gui/controllers/ApartmentsController.java
+++ src/main/java/com/github/lamico/gui/controllers/ApartmentsController.java
@@ -3,9 +3,13 @@ package com.github.lamico.gui.controllers;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.github.lamico.db.DBConnection;
 import com.github.lamico.entities.RentalApartment;
 import com.github.lamico.entities.SaleApartment;
+import com.github.lamico.gui.utils.TextFormatterTypes;
+import com.github.lamico.managers.ResourceManager;
 import com.github.lamico.managers.TabManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -13,18 +17,48 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class ApartmentsController {
+
+	@FXML
+	private AnchorPane root;
 
 	@FXML
 	private Button btRegisterNew;
 
 	@FXML
 	private Accordion acAccordion;
+
+	@FXML
+	private Button btDeleteRent;
+
+	@FXML
+	private Button btDeleteSale;
+
+	@FXML
+	private Button btUpdateRent;
+
+	@FXML
+	private Button btUpdateSale;
+
+	@FXML
+	private RadioButton rbBalconyRent;
+
+	@FXML
+	private RadioButton rbBalconySale;
+
+	@FXML
+	private RadioButton rbGardenRent;
+
+	@FXML
+	private RadioButton rbGardenSale;
 
 	@FXML
 	private TableView<RentalApartment> tbvRentalTable;
@@ -99,13 +133,237 @@ public class ApartmentsController {
 	private TableColumn<?, ?> tvRent;
 
 	@FXML
-	void handleRentalRowSelection(MouseEvent event) {
+	private TextField txtAreaRent;
 
+	@FXML
+	private TextField txtAreaSale;
+
+	@FXML
+	private TextField txtBathRent;
+
+	@FXML
+	private TextField txtBathSale;
+
+	@FXML
+	private TextField txtBedRent;
+
+	@FXML
+	private TextField txtBedSale;
+
+	@FXML
+	private TextField txtCityRent;
+
+	@FXML
+	private TextField txtCitySale;
+
+	@FXML
+	private TextField txtConditionRent;
+
+	@FXML
+	private TextField txtConditionSale;
+
+	@FXML
+	private TextField txtLivingRent;
+
+	@FXML
+	private TextField txtLivingSale;
+
+	@FXML
+	private TextField txtOwnerRent;
+
+	@FXML
+	private TextField txtOwnerSale;
+
+	@FXML
+	private TextField txtPrice;
+
+	@FXML
+	private TextField txtRent;
+
+	@FXML
+	private TextField txtRoomsRent;
+
+	@FXML
+	private TextField txtRoomsSale;
+
+	@FXML
+	private TextField txtStreetRent;
+
+	@FXML
+	private TextField txtStreetSale;
+
+	@FXML
+	private TextField txtUnitRent;
+
+	@FXML
+	private TextField txtUnitSale;
+
+	@FXML
+	void deleteRent(ActionEvent event) {
+		RentalApartment selectedApartment = tbvRentalTable.getSelectionModel().getSelectedItem();
+		if (selectedApartment != null) {
+			Connection connection = DBConnection.getConnection();
+			String checkSaleQuery = "SELECT COUNT(*) FROM SaleApartment WHERE prNum = " + selectedApartment.getPrNum();
+			try (ResultSet resultSet = connection.createStatement().executeQuery(checkSaleQuery)) {
+				resultSet.next();
+				boolean isForSale = resultSet.getInt(1) > 0;
+				String deleteQuery;
+				if (isForSale) {
+					deleteQuery = "DELETE FROM RentalApartment WHERE prNum = " + selectedApartment.getPrNum();
+				} else {
+					deleteQuery = "DELETE FROM Apartment WHERE prNum = " + selectedApartment.getPrNum();
+				}
+				executeQuery(deleteQuery);
+				showRent();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@FXML
+	void deleteSale(ActionEvent event) {
+		SaleApartment selectedApartment = tbvSaleTable.getSelectionModel().getSelectedItem();
+		if (selectedApartment != null) {
+			Connection connection = DBConnection.getConnection();
+			String checkRentQuery = "SELECT COUNT(*) FROM RentalApartment WHERE prNum = "
+					+ selectedApartment.getPrNum();
+			try (ResultSet resultSet = connection.createStatement().executeQuery(checkRentQuery)) {
+				resultSet.next();
+				boolean isForRent = resultSet.getInt(1) > 0;
+				String deleteQuery;
+				if (isForRent) {
+					deleteQuery = "DELETE FROM SaleApartment WHERE prNum = " + selectedApartment.getPrNum();
+				} else {
+					deleteQuery = "DELETE FROM Apartment WHERE prNum = " + selectedApartment.getPrNum();
+				}
+				executeQuery(deleteQuery);
+				showSale();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void executeQuery(String query) throws SQLException {
+		Connection connection = DBConnection.getConnection();
+
+		try (Statement statement = connection.createStatement()) {
+			statement.executeUpdate(query);
+		}
+		connection.close();
+	}
+
+	@FXML
+	void updateRent(ActionEvent event) {
+		RentalApartment selectedApartment = tbvRentalTable.getSelectionModel().getSelectedItem();
+		if (selectedApartment != null) {
+			String updateQuery = "UPDATE Apartment a " + "JOIN RentalApartment ra ON a.prNum = ra.prNum "
+					+ "SET a.roomNum = " + Integer.parseInt(txtRoomsRent.getText()) + ", " + "a.unitNum = "
+					+ Integer.parseInt(txtUnitRent.getText()) + ", " + "a.bedroomNum = "
+					+ Integer.parseInt(txtBedRent.getText()) + ", " + "a.bathroomNum = "
+					+ Integer.parseInt(txtBathRent.getText()) + ", " + "a.livingroomNum = "
+					+ Integer.parseInt(txtLivingRent.getText()) + ", " + "a.hasBalcony = "
+					+ (rbBalconyRent.isSelected() ? 1 : 0) + ", " + "a.kitchenType = '" + txtConditionRent.getText()
+					+ "', " + "a.hasGarden = " + (rbGardenRent.isSelected() ? 1 : 0) + ", " + "ra.rent = "
+					+ Double.parseDouble(txtRent.getText()) + " WHERE a.prNum = " + selectedApartment.getPrNum();
+
+			try {
+				executeQuery(updateQuery);
+				showRent();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@FXML
+	void updateSale(ActionEvent event) {
+		SaleApartment selectedApartment = tbvSaleTable.getSelectionModel().getSelectedItem();
+		if (selectedApartment != null) {
+			String updateQuery = "UPDATE Apartment a " + "JOIN SaleApartment sa ON a.prNum = sa.prNum "
+					+ "SET a.roomNum = " + Integer.parseInt(txtRoomsSale.getText()) + ", " + "a.unitNum = "
+					+ Integer.parseInt(txtUnitSale.getText()) + ", " + "a.bedroomNum = "
+					+ Integer.parseInt(txtBedSale.getText()) + ", " + "a.bathroomNum = "
+					+ Integer.parseInt(txtBathSale.getText()) + ", " + "a.livingroomNum = "
+					+ Integer.parseInt(txtLivingSale.getText()) + ", " + "a.hasBalcony = "
+					+ (rbBalconySale.isSelected() ? 1 : 0) + ", " + "a.kitchenType = '" + txtConditionSale.getText()
+					+ "', " + "a.hasGarden = " + (rbGardenSale.isSelected() ? 1 : 0) + ", " + "sa.price = "
+					+ Double.parseDouble(txtPrice.getText()) + " WHERE a.prNum = " + selectedApartment.getPrNum();
+
+			try {
+				executeQuery(updateQuery);
+				showSale();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@FXML
+	void handleRentalRowSelection(MouseEvent event) {
+		RentalApartment selectedApartment = tbvRentalTable.getSelectionModel().getSelectedItem();
+		if (selectedApartment != null) {
+			Connection connection = DBConnection.getConnection();
+			String query = "SELECT re.prCondition, re.city, re.streetName, re.valuation, re.areaDescription, re.area, re.ownerSSN "
+					+ "FROM RealEstate re, RentalApartment ra " + "WHERE re.prNum = ra.prNum AND ra.prNum = "
+					+ selectedApartment.getPrNum();
+
+			try (ResultSet resultSet = connection.createStatement().executeQuery(query)) {
+				if (resultSet.next()) {
+					txtConditionRent.setText(resultSet.getString("prCondition"));
+					txtCityRent.setText(resultSet.getString("city"));
+					txtStreetRent.setText(resultSet.getString("streetName"));
+					txtPrice.setText(resultSet.getString("valuation"));
+					txtAreaRent.setText(resultSet.getString("areaDescription"));
+					txtAreaRent.setText(resultSet.getString("area"));
+					txtOwnerRent.setText(resultSet.getString("ownerSSN"));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			txtRoomsRent.setText(String.valueOf(selectedApartment.getRoomNum()));
+			txtUnitRent.setText(String.valueOf(selectedApartment.getUnitNum()));
+			txtBedRent.setText(String.valueOf(selectedApartment.getBedroomNum()));
+			txtBathRent.setText(String.valueOf(selectedApartment.getBathroomNum()));
+			txtLivingRent.setText(String.valueOf(selectedApartment.getLivingroomNum()));
+			rbBalconyRent.setSelected(selectedApartment.isHasBalcony());
+			rbGardenRent.setSelected(selectedApartment.isHasGarden());
+			txtRent.setText(String.valueOf(selectedApartment.getRent()));
+		}
 	}
 
 	@FXML
 	void handleSaleRowSelection(MouseEvent event) {
+		SaleApartment selectedApartment = tbvSaleTable.getSelectionModel().getSelectedItem();
+		if (selectedApartment != null) {
+			Connection connection = DBConnection.getConnection();
+			String query = "SELECT re.prCondition, re.city, re.streetName, re.valuation, re.areaDescription, re.area, re.ownerSSN "
+					+ "FROM RealEstate re, SaleApartment sa " + "WHERE re.prNum = sa.prNum AND sa.prNum = "
+					+ selectedApartment.getPrNum();
 
+			try (ResultSet resultSet = connection.createStatement().executeQuery(query)) {
+				if (resultSet.next()) {
+					txtConditionSale.setText(resultSet.getString("prCondition"));
+					txtCitySale.setText(resultSet.getString("city"));
+					txtStreetSale.setText(resultSet.getString("streetName"));
+					txtPrice.setText(resultSet.getString("valuation"));
+					txtAreaSale.setText(resultSet.getString("areaDescription"));
+					txtAreaSale.setText(resultSet.getString("area"));
+					txtOwnerSale.setText(resultSet.getString("ownerSSN"));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			txtRoomsSale.setText(String.valueOf(selectedApartment.getRoomNum()));
+			txtUnitSale.setText(String.valueOf(selectedApartment.getUnitNum()));
+			txtBedSale.setText(String.valueOf(selectedApartment.getBedroomNum()));
+			txtBathSale.setText(String.valueOf(selectedApartment.getBathroomNum()));
+			txtLivingSale.setText(String.valueOf(selectedApartment.getLivingroomNum()));
+			rbBalconySale.setSelected(selectedApartment.isHasBalcony());
+			rbGardenSale.setSelected(selectedApartment.isHasGarden());
+			txtPrice.setText(String.valueOf(selectedApartment.getPrice()));
+		}
 	}
 
 	@FXML
@@ -113,6 +371,34 @@ public class ApartmentsController {
 		showSale();
 		showRent();
 		acAccordion.setExpandedPane(acAccordion.getPanes().get(0));
+		restrictTextFields();
+		root.setBackground(ResourceManager.getBackground("flat.jpg"));
+	}
+
+	private void restrictTextFields() {
+		txtAreaRent.setTextFormatter(TextFormatterTypes.getDecimalTextFormatter(12, 2));
+		txtAreaSale.setTextFormatter(TextFormatterTypes.getDecimalTextFormatter(12, 2));
+		txtBedRent.setTextFormatter(TextFormatterTypes.getIntFormatter(1));
+		txtBedSale.setTextFormatter(TextFormatterTypes.getIntFormatter(1));
+		txtBathRent.setTextFormatter(TextFormatterTypes.getIntFormatter(1));
+		txtBathSale.setTextFormatter(TextFormatterTypes.getIntFormatter(1));
+		txtLivingRent.setTextFormatter(TextFormatterTypes.getIntFormatter(1));
+		txtLivingSale.setTextFormatter(TextFormatterTypes.getIntFormatter(1));
+		txtRoomsRent.setTextFormatter(TextFormatterTypes.getIntFormatter(3));
+		txtRoomsSale.setTextFormatter(TextFormatterTypes.getIntFormatter(3));
+		txtUnitRent.setTextFormatter(TextFormatterTypes.getIntFormatter(6));
+		txtUnitSale.setTextFormatter(TextFormatterTypes.getIntFormatter(6));
+
+		txtCityRent.setTextFormatter(TextFormatterTypes.getAlphaWordCharsFormatter(40));
+		txtCitySale.setTextFormatter(TextFormatterTypes.getAlphaWordCharsFormatter(40));
+		txtConditionRent.setTextFormatter(TextFormatterTypes.getAlphaWordCharsFormatter(255));
+		txtConditionSale.setTextFormatter(TextFormatterTypes.getAlphaWordCharsFormatter(255));
+		txtOwnerRent.setTextFormatter(TextFormatterTypes.getIntFormatter(9));
+		txtOwnerSale.setTextFormatter(TextFormatterTypes.getIntFormatter(9));
+		txtPrice.setTextFormatter(TextFormatterTypes.getDecimalTextFormatter(10, 2));
+		txtStreetRent.setTextFormatter(TextFormatterTypes.getAlphanumericWordCharsAndCommasFormatter(40));
+		txtStreetSale.setTextFormatter(TextFormatterTypes.getAlphanumericWordCharsAndCommasFormatter(40));
+		txtRent.setTextFormatter(TextFormatterTypes.getDecimalTextFormatter(8, 2));
 	}
 
 	private void showRent() throws SQLException {
